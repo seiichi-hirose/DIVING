ca2d2365c9d580a8f2ee013a2f411ba4ef7f7be6
seiichi-hirose <hirose@glocalcode.com>
2023-07-21 16:55:51 +0900

    404はパンくずの色を白にする

--- breadcrumb.php
+++ breadcrumb.php
@@ -1,4 +1,8 @@
+<?php if(is_404()): ?>
+<div class="breadcrumb breadcrumb--white">
+<?php else: ?>
 <div class="breadcrumb">
+<?php endif; ?>
     <?php if (function_exists('bcn_display')) { ?>
         <div id="breadcrumb" class="w_inner" vocab="http://schema.org/" typeof="BreadcrumbList">
             <?php bcn_display(); ?>
