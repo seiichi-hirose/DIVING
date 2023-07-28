848f4085317d61523c8a5c5d7d89e54d40c54e0f
seiichi-hirose <hirose@glocalcode.com>
2023-07-28 19:24:43 +0900

    faqページの繰り返しフィールド設定

--- page-faq.php
+++ page-faq.php
@@ -11,7 +11,7 @@
     <div class="faq__inner inner">
     <?php get_template_part('breadcrumb'); ?>
 
-    <?php
+    <!-- <?php
     $question1 = get_field('question_1');
     $question2 = get_field('question_2');
     $question3 = get_field('question_3');
@@ -19,51 +19,21 @@
     $question5 = get_field('question_5');
     $question6 = get_field('question_6');
     $question7 = get_field('question_7');
-    ?>
+    ?> -->
+
         <div class="faq__wrapper">
-            <?php if($question1): ?>
+        <?php
+        $repetitive_faq = SCF::get('repetitive-faq');
+        foreach ($repetitive_faq as $fields ) {
+        ?>
+
             <dl class="faq__box">
-                <dt class="faq__question js-faq__question"><a><?php echo $question1 ?></a></dt>
-                <dd class="faq__answer"><?php the_field('answer_1'); ?></dd>
+                <dt class="faq__question js-faq__question"><a><?php echo esc_html( $fields['question'] ); ?></a></dt>
+                <dd class="faq__answer"><?php echo esc_html( $fields['answer'] ); ?></dd>
             </dl>
-            <?php endif; ?>
-            <?php if($question2): ?>
-            <dl class="faq__box">
-                <dt class="faq__question js-faq__question"><a><?php echo $question2 ?></a></dt>
-                <dd class="faq__answer"><?php the_field('answer_2'); ?></dd>
-            </dl>
-            <?php endif; ?>
-            <?php if($question3): ?>
-            <dl class="faq__box">
-                <dt class="faq__question js-faq__question"><a><?php echo $question3 ?></a></dt>
-                <dd class="faq__answer"><?php the_field('answer_3'); ?></dd>
-            </dl>
-            <?php endif; ?>
-            <?php if($question4): ?>
-            <dl class="faq__box">
-                <dt class="faq__question js-faq__question"><a><?php echo $question4 ?></a></dt>
-                <dd class="faq__answer"><?php the_field('answer_4'); ?></dd>
-            </dl>
-            <?php endif; ?>
-            <?php if($question5): ?>
-            <dl class="faq__box">
-                <dt class="faq__question js-faq__question"><a><?php echo $question5 ?></a></dt>
-                <dd class="faq__answer"><?php the_field('answer_5'); ?></dd>
-            </dl>
-            <?php endif; ?>
-            <?php if($question6): ?>
-            <dl class="faq__box">
-                <dt class="faq__question js-faq__question"><a><?php echo $question6 ?></a></dt>
-                <dd class="faq__answer"><?php the_field('answer_6'); ?></dd>
-            </dl>
-            <?php endif; ?>
-            <?php if($question7): ?>
-            <dl class="faq__box">
-                <dt class="faq__question js-faq__question"><a><?php echo $question7 ?></a></dt>
-                <dd class="faq__answer"><?php the_field('answer_7'); ?></dd>
-            </dl>
-            <?php endif; ?>
-    </div>
+
+        <?php } ?>
+        </div>
 </div>
 </div>
 </section>
