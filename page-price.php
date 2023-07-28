03d56b85a140b861215ba4923b4aa3ea2b6ae662
seiichi-hirose <hirose@glocalcode.com>
2023-07-28 17:41:04 +0900

    Priceページの内容を編集できるようにコード修正

--- page-price.php
+++ page-price.php
@@ -16,35 +16,35 @@
                             <h2 class="page-price__title">ライセンス講習</h2>
                         </div>
                         <dl class="page-price__body">
-                          <div class="page-price__line"><dt class="page-price__course">オープンウォーター<br class="u-mobile">ダイバーコース</dt><dd class="page-price__value">¥<?php the_field('license_price-1'); ?></dd></div>
-                          <div class="page-price__line"><dt class="page-price__course">アドバンスド<br class="u-mobile">オープンウォーターコース</dt><dd class="page-price__value">¥<?php the_field('license_price-2'); ?></dd></div>
-                          <div class="page-price__line" id="experience"><dt class="page-price__course">レスキュー＋EFRコース</dt><dd class="page-price__value">¥<?php the_field('license_price-3'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('license_text-1'); ?></dt><dd class="page-price__value">¥<?php the_field('license_price-1'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('license_text-2'); ?></dt><dd class="page-price__value">¥<?php the_field('license_price-2'); ?></dd></div>
+                          <div class="page-price__line" id="experience"><dt class="page-price__course"><?php the_field('license_text-3'); ?></dt><dd class="page-price__value">¥<?php the_field('license_price-3'); ?></dd></div>
                         </dl>
                       </div>
                       <div class="page-price__table">
                         <div class="page-price__top"><div class="page-price__image"><img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/price_icon.png" alt="くじらイラスト"></div><h2 class="page-price__title">体験ダイビング</h2></div>
                         <dl class="page-price__body">
-                          <div class="page-price__line"><dt class="page-price__course">ビーチ体験ダイビング<br class="u-mobile">(半日)</dt><dd class="page-price__value">¥<?php the_field('exp_price-1'); ?></dd></div>
-                          <div class="page-price__line"><dt class="page-price__course">ビーチ体験ダイビング<br class="u-mobile">(1日)</dt><dd class="page-price__value">¥<?php the_field('exp_price-2'); ?></dd></div>
-                          <div class="page-price__line"><dt class="page-price__course">ボート体験ダイビング<br class="u-mobile">(半日)</dt><dd class="page-price__value">¥<?php the_field('exp_price-3'); ?></dd></div>
-                          <div class="page-price__line" id="fan"><dt class="page-price__course">ボート体験ダイビング<br class="u-mobile">(1日)</dt><dd class="page-price__value">¥<?php the_field('exp_price-4'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('exp_text-1'); ?></dt><dd class="page-price__value">¥<?php the_field('exp_price-1'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('exp_text-2'); ?></dt><dd class="page-price__value">¥<?php the_field('exp_price-2'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('exp_text-3'); ?></dt><dd class="page-price__value">¥<?php the_field('exp_price-3'); ?></dd></div>
+                          <div class="page-price__line" id="fan"><dt class="page-price__course"><?php the_field('exp_text-4'); ?></dt><dd class="page-price__value">¥<?php the_field('exp_price-4'); ?></dd></div>
                         </dl>
                       </div>
                       <div class="page-price__table">
                         <div class="page-price__top"><div class="page-price__image"><img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/price_icon.png" alt="くじらイラスト"></div><h2 class="page-price__title">ファンダイビング</h2></div>
                         <dl class="page-price__body">
-                          <div class="page-price__line"><dt class="page-price__course">ビーチダイビング<br class="u-mobile">(2ダイブ)</dt><dd class="page-price__value">¥<?php the_field('fan_price-1'); ?></dd></div>
-                          <div class="page-price__line"><dt class="page-price__course">ボートダイビング<br class="u-mobile">(2ダイブ)</dt><dd class="page-price__value">¥<?php the_field('fan_price-2'); ?></dd></div>
-                          <div class="page-price__line"><dt class="page-price__course">スペシャルダイビング<br class="u-mobile">(2ダイブ)</dt><dd class="page-price__value">¥<?php the_field('fan_price-3'); ?></dd></div>
-                          <div class="page-price__line"><dt class="page-price__course">ナイトダイビング<br class="u-mobile">(1ダイブ)</dt><dd class="page-price__value">¥<?php the_field('fan_price-4'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('fan_text-1'); ?></dt><dd class="page-price__value">¥<?php the_field('fan_price-1'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('fan_text-2'); ?></dt><dd class="page-price__value">¥<?php the_field('fan_price-2'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('fan_text-3'); ?></dt><dd class="page-price__value">¥<?php the_field('fan_price-3'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('fan_text-4'); ?></dt><dd class="page-price__value">¥<?php the_field('fan_price-4'); ?></dd></div>
                         </dl>
                       </div>
                       <div class="page-price__table">
                         <div class="page-price__top"><div class="page-price__image"><img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/price_icon.png" alt="くじらイラスト"></div><h2 class="page-price__title">スペシャルダイビング</h2></div>
                         <dl class="page-price__body">
-                          <div class="page-price__line"><dt class="page-price__course">貸切ダイビング<br class="u-mobile">(2ダイブ)</dt><dd class="page-price__value">¥<?php the_field('special_price-1'); ?></dd></div>
-                          <div class="page-price__line"><dt class="page-price__course">1日ダイビング<br class="u-mobile">(3ダイブ)</dt><dd class="page-price__value">¥<?php the_field('special_price-2'); ?></dd></div>
-                          <div class="page-price__line"><dt class="page-price__course">ナイトダイビング<br class="u-mobile">(2ダイブ)</dt><dd class="page-price__value">¥<?php the_field('special_price-3'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('special_text-1'); ?></dt><dd class="page-price__value">¥<?php the_field('special_price-1'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('special_text-2'); ?></dt><dd class="page-price__value">¥<?php the_field('special_price-2'); ?></dd></div>
+                          <div class="page-price__line"><dt class="page-price__course"><?php the_field('special_text-3'); ?></dt><dd class="page-price__value">¥<?php the_field('special_price-3'); ?></dd></div>
                         </dl>
                       </div>
 
