269ff40fb247a5fef985f3e88ee903e9820a6fdc
seiichi-hirose <hirose@glocalcode.com>
2023-08-03 07:15:51 +0900

    メインループ記述

--- page-privacypolicy.php
+++ page-privacypolicy.php
@@ -10,7 +10,10 @@
 <div class="terms__article lower-article">
     <div class="terms__inner inner">
     <?php get_template_part('breadcrumb'); ?>
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <?php the_content(); ?>
+    <?php endwhile; endif; ?>
 
     </div>
 </div>
